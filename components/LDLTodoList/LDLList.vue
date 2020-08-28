0d4f43bcf80d988c4f1726ea544549ce1acdcc5d
langdonglei <langdonglei@sina.com>
2020-08-28 09:04:12 +0800

    m

---
+++ components/LDLTodoList/LDLList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <table v-if="arr.length">
+            <tr>
+                <th>value</th>
+                <th>operate</th>
+            </tr>
+            <tr v-for="(value,index) in arr" :key="index">
+                <td>{{value}}</td>
+                <td>
+                    <button @click="del(index)">del</button>
+                </td>
+            </tr>
+        </table>
+        <div v-else>no data</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LDLList",
+        data() {
+            return {
+                arr: []
+            }
+        },
+        methods: {
+            loadDataFromLocalStorage: function () {
+                // todo {}
+                let data = localStorage.getItem("arr") || {};
+                this.arr = JSON.parse(data);
+            },
+            del(index) {
+                this.arr.splice(index, 1);
+                localStorage.setItem("arr", JSON.stringify(this.arr));
+            }
+        },
+        mounted() {
+            this.loadDataFromLocalStorage();
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/LDLTodoList/LDLTodo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div @keyup.enter="add">
+        <label>
+            <span>id</span>
+            <input type="text" v-model="id">
+        </label>
+        <label>
+            <span>name</span>
+            <input type="text" v-model="name">
+        </label>
+        <label>
+            <span>age</span>
+            <input type="text" v-model="age">
+        </label>
+        <button @click="add">add</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LDLTodo",
+        data: function () {
+            return {
+                id: "",
+                name: "",
+                age: ""
+            }
+        },
+        methods: {
+            add: function () {
+                let row = {id: this.id, name: this.name, age: this.age};
+                let arr = JSON.parse(localStorage.getItem("arr")) || [];
+                arr.push(row);
+                localStorage.setItem("arr", JSON.stringify(arr));
+                this.$emit("localStorageArrChanged");
+            }
+        }
+    }
+</script>
